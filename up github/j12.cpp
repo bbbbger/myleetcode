5c8532d7f2e96549b4553075484827ff8c983373
bbbbger <96936563+bbbbger@users.noreply.github.com>
2022-03-14 18:06:01 +0800

    3.14

--- up github/j12.cpp
+++ up github/j12.cpp
@@ -5,6 +5,30 @@ using namespace std;
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-
+        this->len = word.size();
+        if (len == 0)return false;
+        this->rows = board.size();
+        if (rows == 0)return false;
+        else this->cols = board[0].size();
+        bool ans;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (dfs(board, word, i, j, 0))return true;
+            }
+        }
+        return false;
+    }
+private:
+    int rows, cols;
+    int len;
+    bool dfs(vector<vector<char>>& board, string& word, int i, int j, int k)
+    {
+        if (i >= rows || i < 0 || j >= cols || j < 0 || k >= len || board[i][j] != word[k])return false;
+        if (k == len - 1)return true;
+        board[i][j] = '\0';
+        bool ret = dfs(board, word, i + 1, j, k + 1) || dfs(board, word, i, j + 1, k + 1) ||
+            dfs(board, word, i - 1, j, k + 1) || dfs(board, word, i, j - 1, k + 1);
+        board[i][j] = word[k];
+        return ret;
     }
 };
